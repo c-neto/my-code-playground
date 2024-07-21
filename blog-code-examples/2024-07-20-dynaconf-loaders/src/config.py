b48c8e5dbe78b18c3e097e73e25484b773101f07
Carlos Neto <carlos.neto.dev@gmail.com>
2024-07-21 16:55:17 -0300

    Update config.py

--- blog-code-examples/2024-07-20-dynaconf-loaders/src/config.py
+++ blog-code-examples/2024-07-20-dynaconf-loaders/src/config.py
@@ -3,30 +3,39 @@ from botocore.exceptions import ClientError, BotoCoreError
 from dynaconf import Dynaconf, LazySettings
 import logging
 
-# Configure logging
 logger = logging.getLogger(__name__)
+PARAMETERS_PREFIX = "/my-application/"
 
 
-def _aws_parameter_store_loader(ssm_client, parameters_prefix="/my-application/") -> dict:
+# Retrieve parameters from AWS Parameter Store recursively by prefix
+def _aws_parameter_store_loader(parameters_prefix: str) -> dict:
+    ssm_client = boto3.client('ssm')
+
     paginator = ssm_client.get_paginator("get_parameters_by_path")
+    pages = paginator.paginate(Path=parameters_prefix, Recursive=True, WithDecryption=True)
+
+    parameters_from_aws_raw = {}
+
+    for page in pages:
+        for param in page["Parameters"]:
+            name_full = param["Name"]
+            value = param["Value"]
+            parameters_from_aws_raw[name_full] = value
+
     parameters_from_aws = {}
 
-    for page in paginator.paginate(Path=parameters_prefix, Recursive=True, WithDecryption=True):
-        for param in page["Parameters"]:
-            param_name_full = param["Name"]
-            param_value = param["Value"]
-
-            try:
-                *_, param_name = param_name_full.split("/")
-            except ValueError:
-                logger.warning('Parameter name pattern outside of expected format, skipping: "{param_name_full}"')
-                continue
-
-            parameters_from_aws[param_name] = param_value
+    for name_full, value in parameters_from_aws_raw.items():
+        try:
+            *_, name = name_full.split("/")
+        except ValueError:
+            logger.warning(f'Parameter name pattern outside of expected format, skipping: "{name_full}"')
+        else:
+            parameters_from_aws[name] = value
 
     return parameters_from_aws
 
 
+# Dynaconf callback - used in this case for retrieve parameters from AWS Parameter Store
 def load(obj: LazySettings, env: str, silent: bool = True, key: str = None, filename: str = None) -> None:
     global parameters_from_aws
 
@@ -36,13 +45,14 @@ def load(obj: LazySettings, env: str, silent: bool = True, key: str = None, file
 
 # Attempt to load parameters from AWS Parameter Store and update Dynaconf loaders
 try:
-    ssm_client = boto3.client('ssm')
-    parameters_from_aws = _aws_parameter_store_loader(ssm_client)
+    parameters_from_aws = _aws_parameter_store_loader(PARAMETERS_PREFIX)
 except (BotoCoreError, ClientError) as e:
     LOADERS_FOR_DYNACONF = ["dynaconf.loaders.env_loader"]
     logger.warning("AWS active session not detected, skipping load parameters from AWS Parameter Store")
 else:
+    # __name__ is module name that have "load()" callback. It could be another module...
     LOADERS_FOR_DYNACONF = [__name__, "dynaconf.loaders.env_loader"]
+
 
 # Initialize Dynaconf settings with specified files and loaders
 settings = Dynaconf(
